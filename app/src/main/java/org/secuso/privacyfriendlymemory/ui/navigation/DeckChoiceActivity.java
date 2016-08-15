891de74a5093ebe1e58b53d75286dd1d3b4c4f68
Hannes <Hannes.hofmann92@gmail.com>
2016-08-15 23:01:22 +0200

    add intent for letting the user pick custom images from gallery, not correctly implemented

--- app/src/main/java/org/secuso/privacyfriendlymemory/ui/navigation/DeckChoiceActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlymemory/ui/navigation/DeckChoiceActivity.java
@@ -3,6 +3,7 @@ package org.secuso.privacyfriendlymemory.ui.navigation;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Build;
@@ -11,15 +12,21 @@ import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.util.Log;
 import android.view.MenuItem;
 
+import com.google.android.gms.appindexing.Action;
+
 import org.secuso.privacyfriendlymemory.Constants;
 import org.secuso.privacyfriendlymemory.model.CardDesign;
+import org.secuso.privacyfriendlymemory.model.MemoryDeck;
+import org.secuso.privacyfriendlymemory.model.MemoryDifficulty;
 import org.secuso.privacyfriendlymemory.ui.AppCompatPreferenceActivity;
 import org.secuso.privacyfriendlymemory.ui.R;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -106,6 +113,9 @@ public class DeckChoiceActivity extends AppCompatPreferenceActivity {
         private CheckBoxPreference secondBox = null;
         private CheckBoxPreference thirdBox = null;
 
+        private int PICK_IMAGE_MULTIPLE = 100;
+        private Set<String> customImageUris = new HashSet<>();
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -122,6 +132,67 @@ public class DeckChoiceActivity extends AppCompatPreferenceActivity {
             setupSelection();
             return true;
         }
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            Log.d("DeckChoiceActivity", "In onActivityResult");
+            /*
+            try {
+                // When an Image is picked
+                if (requestCode == PICK_IMAGE_MULTIPLE && resultCode == RESULT_OK
+                        && null != data) {
+                    // Get the Image from data
+
+                    String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                    imagesEncodedList = new ArrayList<String>();
+                    if(data.getData()!=null){
+
+                        Uri mImageUri=data.getData();
+
+                        // Get the cursor
+                        Cursor cursor = getContentResolver().query(mImageUri,
+                                filePathColumn, null, null, null);
+                        // Move to first row
+                        cursor.moveToFirst();
+
+                        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                        imageEncoded  = cursor.getString(columnIndex);
+                        cursor.close();
+
+                    }else {
+                        if (data.getClipData() != null) {
+                            ClipData mClipData = data.getClipData();
+                            ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+                            for (int i = 0; i < mClipData.getItemCount(); i++) {
+
+                                ClipData.Item item = mClipData.getItemAt(i);
+                                Uri uri = item.getUri();
+                                mArrayUri.add(uri);
+                                // Get the cursor
+                                Cursor cursor = getContentResolver().query(uri, filePathColumn, null, null, null);
+                                // Move to first row
+                                cursor.moveToFirst();
+
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                imageEncoded  = cursor.getString(columnIndex);
+                                imagesEncodedList.add(imageEncoded);
+                                cursor.close();
+
+                            }
+                            Log.v("LOG_TAG", "Selected Images" + mArrayUri.size());
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "You haven't picked Image",
+                            Toast.LENGTH_LONG).show();
+                }
+            } catch (Exception e) {
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                        .show();
+            }
+        */
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
 
         private void createOptionsItemListener(){
             Preference setCustomDeckPreference = findPreference("set_custom_deck");
@@ -131,12 +202,20 @@ public class DeckChoiceActivity extends AppCompatPreferenceActivity {
 
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
-                    Set<String> dummyUris = new HashSet<String>();
-                    for(int i = 0; i <= 100; i++){
-                        dummyUris.add(String.valueOf(i));
+                    // let user pick multiple custom images
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent, ""), PICK_IMAGE_MULTIPLE);
+
+                    // check if enough images are picked
+                    int neededImageSize = MemoryDifficulty.Hard.getDeckSize() / 2;
+                    if(customImageUris.size() >= neededImageSize){
+                        sharedPreferences.edit().putStringSet(Constants.CUSTOM_CARDS_URIS, customImageUris).commit();
+                        thirdBox.setEnabled(true);
                     }
-                    sharedPreferences.edit().putStringSet(Constants.CUSTOM_CARDS_URIS, dummyUris).commit();
-                    thirdBox.setEnabled(true);
+
                     return true;
                 }
             });
@@ -145,6 +224,7 @@ public class DeckChoiceActivity extends AppCompatPreferenceActivity {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
                     sharedPreferences.edit().putStringSet(Constants.CUSTOM_CARDS_URIS, new HashSet<String>()).commit();
+                    customImageUris.clear();
                     // refresh selection after custom images are deleted in case of the custom deck was selected
                     if(thirdBox.isChecked()){
                         saveSelectedCardDesign("deck1_key");
