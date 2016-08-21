4a9d47b3712462cb334c435b96d13a48d6e9b66b
Hannes <Hannes.hofmann92@gmail.com>
2016-08-21 19:14:16 +0200

    Removed unused timer delection

--- app/src/main/java/org/secuso/privacyfriendlymemory/model/MemoryTimer.java
+++ app/src/main/java/org/secuso/privacyfriendlymemory/model/MemoryTimer.java
@@ -37,11 +37,6 @@ public class MemoryTimer {
         }
     }
 
-    public void delete(){
-        timerRunning.set(false);
-        internalTimer.cancel();
-    }
-
     public int getTime(){
         return time;
     }
