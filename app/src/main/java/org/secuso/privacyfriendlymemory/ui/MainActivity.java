23352f3e06a0f2b60bb041347bea8e4ba28bea59
Patrick Schneider <urulu@student.kit.edu>
2025-06-11 12:57:20 +0200

    Adds goodbye google dialog.

--- app/src/main/java/org/secuso/privacyfriendlymemory/ui/MainActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlymemory/ui/MainActivity.java
@@ -21,6 +21,7 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 
 import org.secuso.privacyfriendlymemory.Constants;
+import org.secuso.privacyfriendlymemory.GoodbyeGoogleHelperKt;
 import org.secuso.privacyfriendlymemory.R;
 import org.secuso.privacyfriendlymemory.common.MemoGameStatistics;
 import org.secuso.privacyfriendlymemory.common.ResIdAdapter;
@@ -54,6 +55,7 @@ public class MainActivity extends AppCompatDrawerActivity {
         setupViewPager();
         setupDifficultyBar();
         super.setupNavigationView();
+        GoodbyeGoogleHelperKt.checkGoodbyeGoogle(this, getLayoutInflater());
     }
 
     public void setupViewPager() {
